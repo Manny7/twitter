e87493b84a08ba4e80a009bb2069c6f3c3ce94c8
raddek.d@seznam.cz <raddek.d@seznam.cz>
2011-02-18 17:01:01 +0100

    first commit

---
+++ autotwitter.php
@@ -0,0 +1,21 @@
+<?php
+$username = 'RadekDuda';
+$password = 'muhehe2';
+
+        $url = 'http://twitter.com/statuses/update.xml';
+        $curl_handle = curl_init();
+
+        curl_setopt($curl_handle, CURLOPT_URL, "$url");
+        curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 2);
+        curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl_handle, CURLOPT_POST, 1);
+        curl_setopt($curl_handle, CURLOPT_POSTFIELDS, "status=$msg");
+        curl_setopt($curl_handle, CURLOPT_USERPWD, "$username:$password");
+        $buffer = curl_exec($curl_handle);
+        curl_close($curl_handle);
+        if (empty($buffer)) {
+                echo 'fail';
+        } else {
+                echo 'success';
+        }
+?>
